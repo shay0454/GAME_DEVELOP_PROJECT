00ab3191b5cf150ce8b68141723dfba9a2d138f3
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-21 03:06:41 +0900

    스포트라이트 파일 배경 제거, 테스트 용 py파일 추가

---
+++ test.py
@@ -0,0 +1,57 @@
+from pico2d import *
+import os
+os.chdir(os.path.dirname(__file__))
+x,y=400,300
+dir_x,dir_y=0,0
+running=True
+frame=0
+face,updown='',0
+def handle_event():
+    global running, x,y,dir_x,dir_y,face,updown
+    events=get_events()
+    for e in events:
+        if e.type==SDL_KEYDOWN:
+            if e.key==SDLK_ESCAPE:
+                running=False
+            elif e.key==SDLK_d:
+                dir_x+=1
+                face=''
+            elif e.key==SDLK_a:
+                dir_x-=1
+                face='h'
+            elif e.key==SDLK_w:
+                dir_y+=1
+                updown=1
+            elif e.key==SDLK_s:
+                dir_y-=1
+                updown=0
+        elif e.type==SDL_KEYUP:
+            if e.key==SDLK_d:
+                dir_x-=1
+            elif e.key==SDLK_a:
+                dir_x+=1
+            elif e.key==SDLK_w:
+                dir_y-=1
+            elif e.key==SDLK_s:
+                dir_y+=1
+
+def update():
+    global frame,x,y,face
+    frame=(frame+1)%9
+    x+=dir_x*10
+    y+=dir_y*10
+def draw():
+    global frame,x
+    ch.clip_composite_draw((frame//3)*16+18,320+24-updown*24,16,20,0,face,x,y,80,60)
+
+
+open_canvas()
+ch=load_image('Baseball_Players.png')
+while(running):
+    clear_canvas()
+    handle_event()
+    update()
+    draw()
+    update_canvas()
+    delay(0.03)
+close_canvas()
