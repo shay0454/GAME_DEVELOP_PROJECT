de14bd6e083cd03835e7141816292bffb6a7819d
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-21 23:37:24 +0900

    스윙 이미지 데스트

--- test.py
+++ test.py
@@ -37,12 +37,19 @@ def handle_event():
 
 def update():
     global frame,x,y,face
-    frame=(frame+1)%9
+    frame=(frame+1)%6
     x+=dir_x*10
     y+=dir_y*10
-def draw():
+
+def draw(a):
     global frame,x
-    ch.clip_composite_draw((frame//3)*16+18,320+24-updown*24,16,20,0,face,x,y,80,60)
+    if a==1:
+        ch.clip_composite_draw((frame//3)*16+18,320+24-updown*24,16,20,0,face,x,y,80,60)
+    elif a==2:
+        list=[0,2,5,8,12,15]
+        list_1=[16,24,24,24,24,24]
+        list_2=[24,24,0,0,0,24]
+        ch.clip_composite_draw(list[frame]*8,488-72,list_1[frame],32,0,'',x-list_2[frame],y,80*(list_1[frame])/24,60)
 
 
 open_canvas()
@@ -51,7 +58,7 @@ while(running):
     clear_canvas()
     handle_event()
     update()
-    draw()
+    draw(2)
     update_canvas()
-    delay(0.03)
+    delay(0.3)
 close_canvas()
