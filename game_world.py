56fc4ba08285a07ee1d907717f0b6433cc3df371
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-22 23:09:45 +0900

    game_world 추가

---
+++ game_world.py
@@ -0,0 +1,23 @@
+from pico2d import*
+
+objects=[[],[],[]]
+
+def add_object(o,depth=2):
+    objects[depth].append(o)
+
+def update():
+    for layer in objects:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in objects:
+        for o in layer:
+            o.draw()
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            return
+    return ValueError("does not exist")
