fb0f110ffccdc227e962318baaeb422c6b821a1a
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-11-07 19:44:07 +0900

    main에 game_framework 도입

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import pico2d
-import play_mode
+from pico2d import *
+import logo_mode as start_mode
+import game_framework
 import os
 os.chdir(os.path.dirname(__file__))
 
-pico2d.open_canvas()
-play_mode.create_world()
-while play_mode.running:
-    play_mode.update_world()
-    play_mode.render_world()
-    play_mode.handle_events()
-    play_mode.check_players()
-    pico2d.delay(0.01)
-
-play_mode.finish()
-
-pico2d.close_canvas()
+open_canvas()
+game_framework.run(start_mode)
+close_canvas()
