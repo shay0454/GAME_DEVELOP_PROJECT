60b448bd415a60d411c2f02fee95b959835f456f
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-11-08 09:46:50 +0900

    start 버튼 이식

--- title_mode.py
+++ title_mode.py
@@ -2,9 +2,10 @@ from pico2d import *
 import game_framework
 import play_mode
 def init():
-    global image,temp
+    global image,temp,button
     image=load_image('white_background.png')
     temp=load_font('ENCR10B.TTF', 50)
+    button=load_image('start_button.png') #https://www.flaticon.com/free-icon/start-button_5261929
 
 def finish():
     global temp,image
@@ -14,7 +15,7 @@ def handle_events():
     events=get_events()
     for event in events:
         if event.type==SDL_MOUSEBUTTONDOWN:
-            if(event.x>=300 and event.x<=500 and event.y<=600-100-1 and event.y>=600-200-1):
+            if(event.x>=300 and event.x<=500 and event.y<=600-50-1 and event.y>=600-250-1):
                 game_framework.change_mode(play_mode)
         elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
             game_framework.quit()
@@ -26,6 +27,6 @@ def update():
 def draw():
     clear_canvas()
     image.draw(400,300)
-    temp.draw(275,450,'BASEBALL')
-    temp.draw(275,400,'  GAME  ')
+    temp.draw(275,450,'BASEBALL\n  GAME  ')
+    button.draw(400,200,400,300)
     update_canvas()
