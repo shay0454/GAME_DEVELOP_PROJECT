7e002c5afb60576c1f057a371dbd382e5b54be01
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-11-24 01:29:57 +0900

    공의 속도, 가속도 임의 구현, 상태머신 다시 활성화

--- ball.py
+++ ball.py
@@ -5,19 +5,26 @@ import play_mode
 import random
 def is_catch(ball):
     pass
+def is_stopped(ball):
+    return ball.pixel_v==0
 
-PIXEL_PER_METER=5.9
+def is_move(ball):
+    return ball.pixel_v!=0
+def is_passed(ball):
+    return False
+PIXEL_PER_METER=8.16
 PI=math.pi
 TIME_PER_ACTION=1
 ACTION_PER_TIME=1/TIME_PER_ACTION
 FRAME_PER_ACTION=200
 class Stopped:
     @staticmethod
-    def enter(ball,e):
+    def enter(ball):
+        print('stop')
         pass
 
     @staticmethod
-    def exit(ball,e):
+    def exit(ball):
         pass
 
     @staticmethod
@@ -30,12 +37,12 @@ class Stopped:
 
 class Moving:
     @staticmethod
-    def enter(ball,e):
-        print('ball:move',run_speed_KMPH)
+    def enter(ball):
+        print('ball:move',ball.pixel_v)
         pass
 
     @staticmethod
-    def exit(ball,e):
+    def exit(ball):
         pass
 
     @staticmethod
@@ -48,11 +55,11 @@ class Moving:
 
 class Catched:
     @staticmethod
-    def enter(ball,e):
+    def enter(ball):
         pass
 
     @staticmethod
-    def exit(ball,e):
+    def exit(ball):
         pass
 
     @staticmethod
@@ -65,9 +72,9 @@ class Catched:
 class StateMachine:
     def __init__(self,ball):
         self.ball=ball
-        self.cur_state=Stopped
+        self.cur_state=Moving
         self.state_table={
-            Stopped : {is_catch:Catched},
+            Stopped : {is_catch:Catched,is_move:Moving},
             Moving:{is_stopped:Stopped},
             Catched:{is_passed:Moving}
         }
@@ -77,12 +84,13 @@ class StateMachine:
 
     def update(self):
         self.cur_state.do(self.ball)
+        self.handle_event()
 
     def draw(self):
         self.cur_state.draw(self.ball)
 
     def handle_event(self):
-        for ckeck_event, next_state in self.state_table[self.cur_state]:
+        for ckeck_event, next_state in self.state_table[self.cur_state].items():
             if ckeck_event(self.ball):
                 self.cur_state.exit(self.ball)
                 self.cur_state=next_state
@@ -93,18 +101,32 @@ class StateMachine:
 
 class Ball:
     def __init__(self,x,y,shoot_v,angle):
-        global RUN_SPEED_KMPH
         self.image=load_image('ball.png')
-        self.cur_state=Moving
-        self.set_v(shoot_v)
+        self.state_machine=StateMachine(self)
+        self.pixel_v=shoot_v*self.set_v_1kmph()
         self.shoot_angle=angle
         self.x,self.y=x,y
-        self.heigt=1.5 #meter
-        self.cur_state.enter(self,None)
+        self.a=-4*8  # 1m/s^2*PIXEL_PER_METER=4.08 pixel
+        self.height_a=-80
+        self.height_v=30
+        self.height=1.5*8.16 #meter   1.5*PIXEL_PER_METER
+        self.state_machine.start()
 
     def update(self):
-        self.x+=game_framework.frame_time*run_speed_PPS*math.cos(self.shoot_angle)
-        self.y+=game_framework.frame_time*run_speed_PPS*math.sin(self.shoot_angle)
+        self.state_machine.update()
+        if self.height>0:
+            self.height+=self.height_a*game_framework.frame_time
+        else:
+            self.heihgt=0
+            self.a=-4*32
+        self.height+=game_framework.frame_time*self.height_v
+        if self.pixel_v>0:
+            self.pixel_v+=self.a*game_framework.frame_time
+        else:
+            self.pixel_v=0
+        self.x+=game_framework.frame_time*self.pixel_v*math.cos(self.shoot_angle)
+        self.y+=game_framework.frame_time*self.pixel_v*math.sin(self.shoot_angle)
+        
 
     
     def draw(self):
@@ -116,12 +138,15 @@ class Ball:
     
     def handle_collision(self,group,other):
         if group=='ball:bat':
+            self.pixel_v=160*self.set_v_1kmph()
             self.shoot_angle=play_mode.control.players[-1].bat.angle+PI/2+PI*1/12*random.random()
 
-    def set_v(self,v):
-        global run_speed_KMPH,run_speed_PPS
-        run_speed_KMPH=v
+
+    def set_v_1kmph(self):
+        run_speed_KMPH=1
         run_speed_MPM=(run_speed_KMPH*1000/60)
         run_speed_MPS=(run_speed_MPM/60)
         run_speed_PPS=(run_speed_MPS*PIXEL_PER_METER)
+        return run_speed_PPS
+
     
