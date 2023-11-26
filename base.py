3408a626eb9f61ebed21d648e3d660f0c0e8056c
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-11-27 04:20:47 +0900

    각 base에 번호부여 collision일때 player의 target_base와 같으면 다음 베이스로 셋팅

--- base.py
+++ base.py
@@ -1,8 +1,10 @@
 from pico2d import draw_rectangle
 import game_world
+import play_mode
 class Base_p:
-    def __init__(self,x,y):
+    def __init__(self,x,y,num):
         self.size=[48,48]
+        self.num=num
         self.x,self.y=x,y
         self.is_death=False
         self.player=None
@@ -14,7 +16,6 @@ class Base_p:
     def update(self):
         if self.player!=None and not game_world.collide(self,self.player):
             self.player=None
-            self.player.check_base=False
 
     def handle_collision(self,group,other):
         if group=='base:player':
@@ -24,14 +25,15 @@ class Base_p:
 class Base:
     def __init__(self):
         self.base_locations={'base1':[560+4,240],'base2':[400,400+12],'base3':[240-4,240],'base4':[400,90]}
-        self.bases=[Base_p(*self.base_locations['base'+str(i+1)]) for i in range(4)]
+        self.bases=[Base_p(*self.base_locations['base'+str(i+1)],(i+1)%4) for i in range(4)]
 
     def players_run(self):
          for i in range(3,-1,-1):
-            if self.bases[i].player != None and self.bases[i].player.check_base:
+            if self.bases[i].player != None and self.bases[i].player.target_base%4==self.bases[i].num:
                 self.bases[i].player.target_base+=1
                 the_base='base'+str(self.bases[i].player.target_base)
                 self.bases[i].player.goto(self.base_locations[the_base])
+                self.bases[i].player.state_machine.change_state(play_mode.control.player_state_list['Idle'],('Change',0))
 
     def player_run(self,player):
         player.target_base+=1
