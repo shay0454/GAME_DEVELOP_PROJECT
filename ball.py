4942bd27bba154e396b436bd0d625da3c069f5a5
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-12-01 11:46:16 +0900

    탄성계수 도입

--- ball.py
+++ ball.py
@@ -110,57 +110,60 @@ class Ball:
         self.shadow_image=load_image('ball_shadow.png')
         self.state_machine=StateMachine(self)
         self.v,self.static_v,self.h_v=shoot_v,shoot_v,shoot_v*3/4
-        self.a,self.h_a=-6*8,-80
+        self.a,self.h_a=-5*8,-80
         self.distance=self.v*(self.v/(-self.a))/2
-        self.shoot_angle=angle
-        self.x,self.y,self.h=x,y,4
+        self.shoot_angle,self.h_angle=angle,0
+        self.x,self.y,self.h=x,y,12
+        self.is_hit=False
         self.cur_state=0
         self.moving_state=['sky','allow','on_ground','stop']
         self.state_point=[]
         self.state_machine.start()
-        self.hit_tf=False
 
     def is_less_than(self):
         return (self.destination[0]-self.x)**2+(self.destination[1]-self.y)**2<=(game_framework.frame_time*ONE_PPS)**2
 
     def update(self):
-        if self.h<=0:
-            self.h=0
-            self.h_v=0
-        else:
-            self.h_v+=self.h_a*game_framework.frame_time
-        if self.v<=0:
-            self.v=0
-        else:
-            self.v+=self.a*game_framework.frame_time
-        self.x+=game_framework.frame_time*ONE_PPS*self.v*math.cos(self.shoot_angle)
-        self.y+=game_framework.frame_time*ONE_PPS*self.v*math.sin(self.shoot_angle)
-        self.h+=game_framework.frame_time*ONE_PPS*self.h_v
+        if self.is_hit:
+            self.set_velocity_h()
+            self.h+=game_framework.frame_time*ONE_PPS*self.h_v*math.sin(self.h_angle)
+        self.set_velocity_xy()
+        self.x+=game_framework.frame_time*ONE_PPS*self.v*math.cos(self.shoot_angle)*math.cos(self.h_angle)
+        self.y+=game_framework.frame_time*ONE_PPS*self.v*math.sin(self.shoot_angle)*math.cos(self.h_angle)
 
     
     def draw(self):
         self.shadow_image.clip_draw(0,0,68,68,self.x,self.y,4,2)
-        if self.hit_tf:
-            self.image.clip_draw(0,0,68,68,self.x,self.y+self.h,4,4)
-        else:
-            self.image.clip_draw(0,0,68,68,self.x,self.y,4,4)
+        self.image.clip_draw(0,0,68,68,self.x,self.y+self.h,4,4)
         draw_rectangle(*self.get_bb())
 
-    def under_zero(self,v):
-        if v<0: 
-            v=0
-        return v
-
     def get_bb(self):
-        return self.x-2,self.y-2,self.x+2,self.y+2
+        return self.x-2,self.y+self.h-2,self.x+2,self.y+self.h+2
     
     def handle_collision(self,group,other):
         if group=='ball:bat':
-            self.hit_tf=True
-            play_mode.control.state_machine.change_state(play_mode.control.state_list['Hitted'])
+            if not self.is_hit:
+                play_mode.control.state_machine.change_state(play_mode.control.state_list['Hitted'])
+                self.shoot_angle=play_mode.control.runner[-1].bat.rad+PI/2+PI*1/12*random.random()
+                play_mode.control.runner[-1].state_machine.change_state(play_mode.control.player_state_list['Idle'])
             self.v=random.randint(140,170)
-            self.destination=[self.x+self.distance*math.cos(self.shoot_angle),self.y+self.distance*math.sin(self.shoot_angle)]
-            self.shoot_angle=play_mode.control.players[-1].bat.angle+PI/2+PI*1/12*random.random()
+            self.h_v=self.v*math.sin(random.randint(25,35))
+            self.h_angle=PI/180*random.randint(25,35)
+            self.is_hit=True
+
+    def set_velocity_h(self):
+        if self.h<0:
+            self.h=0
+            self.h_v*=-0.5 if (abs(self.h_v)>20)else 0
+        else:
+            self.h_v+=self.h_a*game_framework.frame_time
+    
+    def set_velocity_xy(self):
+        if self.v<=0:
+            self.v=0
+        else:
+            self.v+=self.a*game_framework.frame_time
+    
 
 
     
