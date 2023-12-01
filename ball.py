77d45e35f4750a104e0ef77aa6eea3ac2aa8c1e9
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-12-01 16:10:31 +0900

    개선 및 fielder들 위한 데이터 관련 함수 생성

--- ball.py
+++ ball.py
@@ -110,27 +110,22 @@ class Ball:
         self.shadow_image=load_image('ball_shadow.png')
         self.state_machine=StateMachine(self)
         self.v,self.static_v,self.h_v=shoot_v,shoot_v,shoot_v*3/4
-        self.a,self.h_a=-5*8,-80
-        self.distance=self.v*(self.v/(-self.a))/2
+        self.a,self.h_a=-5*8,-100
         self.shoot_angle,self.h_angle=angle,0
         self.x,self.y,self.h=x,y,12
         self.is_hit=False
         self.cur_state=0
         self.moving_state=['sky','allow','on_ground','stop']
-        self.state_point=[]
+        self.state_point,self.times_when_ball_on_ground,self.distances_when_ball_on_ground,self.locations_when_ball_on_ground=[],[],[],[]
         self.state_machine.start()
+        play_mode.control.ball=self
 
     def is_less_than(self):
         return (self.destination[0]-self.x)**2+(self.destination[1]-self.y)**2<=(game_framework.frame_time*ONE_PPS)**2
 
     def update(self):
-        if self.is_hit:
-            self.set_velocity_h()
-            self.h+=game_framework.frame_time*ONE_PPS*self.h_v*math.sin(self.h_angle)
-        self.set_velocity_xy()
-        self.x+=game_framework.frame_time*ONE_PPS*self.v*math.cos(self.shoot_angle)*math.cos(self.h_angle)
-        self.y+=game_framework.frame_time*ONE_PPS*self.v*math.sin(self.shoot_angle)*math.cos(self.h_angle)
-
+        self.update_z()
+        self.update_xy()
     
     def draw(self):
         self.shadow_image.clip_draw(0,0,68,68,self.x,self.y,4,2)
@@ -143,27 +138,62 @@ class Ball:
     def handle_collision(self,group,other):
         if group=='ball:bat':
             if not self.is_hit:
+                self.calculate_times()
+                self.set_per_distance()
+                play_mode.control.allow_catch_list=[self.times_when_ball_on_ground,self.distances_when_ball_on_ground,self.locations_when_ball_on_ground]
                 play_mode.control.state_machine.change_state(play_mode.control.state_list['Hitted'])
                 self.shoot_angle=play_mode.control.runner[-1].bat.rad+PI/2+PI*1/12*random.random()
                 play_mode.control.runner[-1].state_machine.change_state(play_mode.control.player_state_list['Idle'])
             self.v=random.randint(140,170)
-            self.h_v=self.v*math.sin(random.randint(25,35))
+            self.static_v=self.v
             self.h_angle=PI/180*random.randint(25,35)
+            self.h_v,self.v=self.v*math.sin(self.h_angle),self.v*math.cos(self.h_angle)
             self.is_hit=True
 
-    def set_velocity_h(self):
+    def update_xy(self):
+        self.set_xy_velocity()
+        self.x+=game_framework.frame_time*ONE_PPS*self.v*math.cos(self.shoot_angle)
+        self.y+=game_framework.frame_time*ONE_PPS*self.v*math.sin(self.shoot_angle)
+
+    def update_z(self):
+        if self.is_hit:
+            self.set_z_velocity()
+            self.h+=game_framework.frame_time*ONE_PPS*self.h_v
+
+    def set_z_velocity(self):
         if self.h<0:
             self.h=0
-            self.h_v*=-0.5 if (abs(self.h_v)>20)else 0
+            self.h_v*=-0.5 if (abs(self.h_v/self.h_a)<abs(self.v/self.a))else 0
         else:
             self.h_v+=self.h_a*game_framework.frame_time
     
-    def set_velocity_xy(self):
+    def set_xy_velocity(self):
         if self.v<=0:
             self.v=0
         else:
             self.v+=self.a*game_framework.frame_time
     
+#a(t)=a
+#v(t)=v+a*t
+#x(t)=1/2*a*t^2+v*t+x
+#12=1/2*a*t^2+v*t+x
+#v(t')=0 -> v=-a*t
+#x(t')=1/2*a*t^2-a
+# (a+v+a*t)*t*1/2-12=0
+# (80+
+    def calculate_times(self):
+        the_height_v=self.h_v
+        time=abs(2*self.h_v/self.h_a)
+        i,t=1,1
+        while the_height_v>40:
+            self.times_when_ball_on_ground.append(time*i)
+            t,i,the_height_v=t/2,i+t,the_height_v/2
+        self.times_when_ball_on_ground.append(abs(self.v/self.a))
 
-
+    def set_per_distance(self):
+        for t in self.times_when_ball_on_ground:
+            distance=abs(self.a/2*t**2+self.static_v*t)
+            location=[self.x+distance*math.cos(self.shoot_angle),self.y+distance*math.sin(self.shoot_angle)]
+            self.distances_when_ball_on_ground.append(distance)
+            self.locations_when_ball_on_ground.append(location)
     
